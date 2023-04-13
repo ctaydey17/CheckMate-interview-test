bc317d63e87827e06617fc36945534efa4b0d5f9
ctaydey17 <taydey.charline@gmail.com>
2023-04-13 21:47:20 +0800

    finished!

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,29 @@
 import Head from 'next/head'
 import GoogleButton from 'react-google-button'
-import {getAuth, GoogleAuthProvider} from "firebase/auth";
-import {useRouter} from "next/router";
+import { getAuth, GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
+import { useRouter } from "next/router";
 import { initializeApp } from 'firebase/app';
+import { useEffect } from 'react';
 
 // Task 0: Initialize Firebase
 // Replace the following with your app's Firebase project configuration
 // https://firebase.google.com/docs/web/setup
 const firebaseConfig = {
   // Enter your own firebase config here
+  apiKey: "AIzaSyCLtDNcGu4jQa_508HsrR-GjnBTi0nRwnE",
+  authDomain: "checkmate-test-b1fd7.firebaseapp.com",
+  projectId: "checkmate-test-b1fd7",
+  storageBucket: "checkmate-test-b1fd7.appspot.com",
+  messagingSenderId: "933920808201",
+  appId: "1:933920808201:web:5f89e53f5a3357a6ccc7d8"
 };
 
-// const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 // GoogleAuthProvider instance
-// const provider = new GoogleAuthProvider();
+const provider = new GoogleAuthProvider();
 // Firebase Auth instance
-// const auth = getAuth(app);
+const auth = getAuth(app);
 
 export default function Home() {
   //Next.js router
@@ -24,15 +31,37 @@ export default function Home() {
 
   // Task 1: Implement Google Sign in with Firebase
   // https://firebase.google.com/docs/auth/web/google-signin
-  const signIn = () => {
-    /*
-      1. Use the GoogleAuthProvider to sign in with Firebase
-      2. Use signInWithRedirect to redirect the user to the Google sign in page
-      3. (Optional) Use getRedirectResult to get the result of the redirect and check out what is inside :)
-      4. Redirect the user to the signed-in page using Next.js router
-     */
-
+  // console.log("usdshu")
+  const signIn = async () => {
+    try {
+      await signInWithRedirect(auth, provider);
+    } catch (err: any) {
+      console.error(err);
+      alert(err.message);
+    }
   }
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        console.log(user.uid);
+        router.push("/signed-in");
+      } else {
+        console.log("not logged in");
+        // Handle the case where the user is not logged in
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  //   /*
+  //     1. Use the GoogleAuthProvider to sign in with Firebase
+  //     2. Use signInWithRedirect to redirect the user to the Google sign in page
+  //     3. (Optional) Use getRedirectResult to get the result of the redirect and check out what is inside :)
+  //     4. Redirect the user to the signed-in page using Next.js router
+  //    */
+
 
   return (
     <>
@@ -41,7 +70,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <div className="container">
-        <main style={{display: 'flex', justifyContent:'center', alignItems:'center', flexDirection: 'column'}}>
+        <main style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
           <h1 className="title">
             Welcome to <a href="https://checkmatehk.io">CheckMate</a>
           </h1>
@@ -52,11 +81,11 @@ export default function Home() {
           <GoogleButton
             label={'Sign in with Google'}
             type="light"
-            style={{ width: '50%', display:"flex", justifyContent: 'center', alignItems: 'center', fontFamily: 'Roboto, sans-serif', color:'#444' }}
+            style={{ width: '50%', display: "flex", justifyContent: 'center', alignItems: 'center', fontFamily: 'Roboto, sans-serif', color: '#444' }}
             onClick={signIn}
           />
         </main>
       </div>
-      </>
+    </>
   )
 }
